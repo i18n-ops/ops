4c890e35c9c8bcfae2c9745272b9cea9d8b41e65
i18n <i18n.site@gmail.com>
2025-02-11 15:32:22 +0800

    ♨

--- ansible/ubuntu/setup/nvim.sh
+++ ansible/ubuntu/setup/nvim.sh
@@ -31,8 +31,6 @@ vi -E -u /etc/vim/sysinit.vim +PlugInstall +qa
 
 vi -u /etc/vim/sysinit.vim +UpdateRemotePlugins +qa
 
-vi +'CocInstall -sync coc-biome coc-rust-analyzer coc-json coc-yaml coc-css coc-python coc-vetur coc-svelte' +qa
-
 if [ "$(uname -m)" != "aarch64" ]; then
   vi +'CocInstall -sync coc-tabnine' +qa
 fi
