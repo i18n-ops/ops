592c467a225e020ed1b832ac2fe5d735f7c19d32
i18n <i18n.site@gmail.com>
2025-09-28 16:09:54 +0800

    ♨

--- ipv6_proxy/init.os.sh
+++ ipv6_proxy/init.os.sh
@@ -18,28 +18,34 @@ if [ ! -f "$add_route" ]; then
     apt install -y networkd-dispatcher
   fi
 
-  ipv6=$(ip -6 addr show dev eth0 | grep "inet6.*scope global" | awk '{print $2}' | awk -F'/' '{print $1}' | awk -F':' '{print $1":"$2":"$3":"$4"::/64"}')
+  ipv6=$(ip -6 addr show dev eth0 | grep "inet6.*scope global" | awk '{print $2}' | awk -F'/' '{print $1}' | awk -F':' '{print $1":"$2":"$3":"$4"::"}')
 
   cat <<EOF >$add_route
 #!/bin/sh
 
 if [ "\$IFACE" = "eth0" ]; then
-    ip route add local $ipv6 dev eth0
+    ip route add local $ipv6/64 dev eth0
 fi
 EOF
-
   chmod +x $add_route
 
-  apt-get install -y ndppd
+fi
 
-  # netcup 需要这个 ， contablo 不需要这个
+systemctl enable --now networkd-dispatcher || true
+systemctl restart networkd-dispatcher
+
+# netcup 需要这个 ， contabo 不需要这个
+if curl --interface ${ipv6}f  -IsS --max-time 10 https://baidu.com >/dev/null 2>&1; then
+  echo "不需要 ndppd"
+else
+  apt-get install -y ndppd
   cat <<EOF >/etc/ndppd.conf
 route-ttl 30000
 proxy eth0 {
     router no
     timeout 500
     ttl 30000
-    rule $ipv6 {
+    rule $ipv6/64 {
       static
     }
 }
@@ -47,6 +53,3 @@ EOF
   systemctl enable --now ndppd || true
   systemctl restart ndppd || true
 fi
-
-systemctl enable --now networkd-dispatcher || true
-systemctl restart networkd-dispatcher
